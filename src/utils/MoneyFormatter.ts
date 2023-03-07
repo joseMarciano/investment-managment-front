02278627c37ee32ac6c8423b20806f2d4ffadafd
joseMarciano <marcianojosepaulo@gmail.com>
2023-03-06 23:22:37 -0300

    feat: :wrench: create helper class to format money

---
+++ src/utils/MoneyFormatter.ts
@@ -0,0 +1,10 @@
+
+const shortBrlFormatter = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
+
+export class MoneyFormatter {
+
+    static shortBRL(value: number): string {
+        return value ? shortBrlFormatter.format(value) : '';
+    }
+
+}
